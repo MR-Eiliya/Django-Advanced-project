2e26d818aaa87e5e19641989f0ad82ba41f12025
Mr.EIliya <mreiliya333@gmail.com>
2025-12-18 21:07:51 +0330

    reformatted for flake8 for second time

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,11 +1,10 @@
 from rest_framework.permissions import (
-    IsAuthenticated,
-    IsAuthenticatedOrReadOnly,
+    IsAuthenticatedOrReadOnly
 )
 from .serializers import PostSerializers, CategorySerializer
 from ...models import Post, Category
 from rest_framework import viewsets
-from .permissions import IsOwnerOrReadOnly
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .paginations import DefaultPagination
@@ -13,7 +12,7 @@ from .paginations import DefaultPagination
 
 # viewset example for cbv
 class PostModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    #permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializers
     queryset = Post.objects.filter(status=True)
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
